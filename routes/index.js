a66a492dfbd1c22a319b2ada5606b23962b609be
root <root@altonji.com>
2015-07-08 16:26:41 -0400

    lots of small changes

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,27 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next_) {
     if (req.body.attachments) {
-        req.body.attachments.forEach(function(attachment) {
+        console.log(req.body);
+	var message = req.body.text;
+	req.body.attachments.forEach(function(attachment) {
             if (attachment.type == "mentions") {
-                console.log("mentions " + attachment.user_ids + " at " + attachment.loci);
+		var i;
+                for (i = 0; i < attachment.user_ids.length; i++) {
+		    var user_id = attachment.user_ids[i];
+		    var loc = attachment.loci[i]; 
+		    var name = message.substring(loc[0], loc[0] + loc[1]);
+		    console.log("mentions " + user_id + " or " + name + "  at " + loc);
+		    var j;
+		    for (j = (loc[0] + loc[1]); j < messsage.length; j++) {
+			var c = message[j];
+			if(c == '+') {
+			    if ((j + 1) < message.length) {
+				var nextc = message[j + 1];
+			    	if (nextc == '+') console.log("add");
+			    }
+			}
+		    }
+		}
             }
         });
     }
